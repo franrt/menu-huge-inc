6274a275bfc7d69c869add3dfc72c2dead210f09
Fran Ruiz-Tagle <francisca.ruiztagle@gmail.com>
2017-05-11 22:37:59 -0300

    funciona hasta slide 34 ok

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,14 @@ document.getElementById("toggle").addEventListener("click", function(event) {
     document.getElementById("header").classList.toggle("open");
     document.getElementById("body").classList.toggle("overflow-hidden");
 });
+
+var lastScrollTOp = 0;
+window.addEventListener("scroll", function() {
+    var currentScroll = window.pageYOffset || document.body.scrollTop;
+    if (currentScroll > lastScrollTOp) {
+        console.log("down"); // down
+    } else {
+        console.log("up"); //up
+    }
+    lastScrollTOp = currentScroll; //ignorar por ahora -_-
+}, false);
